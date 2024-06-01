1ca801abee60a86cfdba34b9c838d9ca828b04f5
preethi <selvampreethi360@gmail.com>
2024-06-01 14:04:18 +0530

    make section as link container

--- js/links.js
+++ js/links.js
@@ -1,10 +1,15 @@
 const footerCTA = document.querySelector("footer .footer__cta");
+const modern_layout_link = document.querySelector(".highlight__link");
 const link01 = document.querySelector(".modal-links .modal-link:nth-child(1)");
 const link02 = document.querySelector(".modal-links .modal-link:nth-child(2)");
 const link03 = document.querySelector(".modal-links .modal-link:nth-child(3)");
 
 footerCTA.addEventListener("click", (_) => {
   window.location.href = "mailto:selvampreethi360@gmail.com";
+});
+
+modern_layout_link.addEventListener("click", (_) => {
+  window.location.href = "https://github.com/Preethi-Dev/modern-layouts";
 });
 
 link01.addEventListener("click", (_) => {
